0d8d48708f83dab3bfb838469a79779b76777083
masyaka.94 <masyaka.94@gmail.com>
2022-10-30 13:36:03 +0500

    Zadacha60

---
+++ Zadacha60/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.*/
+
+void FillArray(int[,,] matr)
+{
+    int Element = 10;
+    
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            matr[i, j, k] = Element++;
+        }
+    }
+}
+
+void PrintArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {Console.Write($"{matr[i, j, k]} [{i},{j},{k}] ");}
+            Console.WriteLine();
+        }
+    }
+}
+int[,,] matrix = new int[2,2,2];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
